1e6ee45e27786e668fd262ef67d9bc88582d4298
Jules Chendjou <julestl@yahoo.fr>
2023-03-04 20:09:20 -0500

    Update

--- SeleniumcoreDemo/UnitTest1.cs
+++ SeleniumcoreDemo/UnitTest1.cs
@@ -9,17 +9,33 @@ namespace SeleniumcoreDemo
         [SetUp]
         public void Setup()
         {
-           
+            Console.WriteLine("is it executing first");
         }
 
         [Test]
         public void Test1()
         {
             //       Open the driver 
-            IWebDriver webDriver = new ChromeDriver(@"C:\Users\\jules\selenium\test\SeleniumcoreDemo");
+            IWebDriver webDriver = new ChromeDriver();
 
             // Navigate to site 
             webDriver.Navigate().GoToUrl("http://eaapp.somee.com/");
+            // login
+            IWebElement InkLogin = webDriver.FindElement(By.LinkText("Login"));
+            InkLogin.Click();
+            // Username
+            var txtUserName = webDriver.FindElement(By.Name("UserName"));
+            // Assertion
+
+            Assert.That(txtUserName.Displayed, Is.True);
+
+            txtUserName.SendKeys("admin");
+            webDriver.FindElement(By.Name("Password")).SendKeys("password");
+            webDriver.FindElement(By.XPath("//input[@value='Log in']")).Submit();
+            var InkEmployeeDetails = webDriver.FindElement(By.LinkText("Employee Details"));
+            Assert.That(InkEmployeeDetails.Displayed, Is.True);
+
+
         }
     }
 }
