1043acffe71a9dd0cca8675d8d00623df8fabe1a
Jules Chendjou <julestl@yahoo.fr>
2023-03-06 15:43:26 -0500

    Update fix the headless mode

--- SeleniumcoreDemo/UnitTest1.cs
+++ SeleniumcoreDemo/UnitTest1.cs
@@ -20,33 +20,45 @@ namespace SeleniumcoreDemo
             //       Open the driver 
             var ChromeOptions = new ChromeOptions();
             ChromeOptions.AddArgument("headless");
-            
-                IWebDriver webDriver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
 
-            // Navigate to site 
-            webDriver.Navigate().GoToUrl("http://eaapp.somee.com/");
-            // login
-            IWebElement InkLogin = webDriver.FindElement(By.LinkText("Login"));
-            InkLogin.Click();
-            // Username
-            var txtUserName = webDriver.FindElement(By.Name("UserName"));
-            // Assertion
+            //IWebDriver webDriver = new ChromeDriver();
+            using (var driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), ChromeOptions))
+            {
 
-            Assert.That(txtUserName.Displayed, Is.True);
+                // Navigate to site 
 
-            txtUserName.SendKeys("admin");
-            webDriver.FindElement(By.Name("Password")).SendKeys("password");
-            webDriver.FindElement(By.XPath("//input[@value='Log in']")).Submit();
-            var InkEmployeeDetails = webDriver.FindElement(By.LinkText("Employee Details"));
-            Assert.That(InkEmployeeDetails.Displayed, Is.True);
+                //webDriver.Navigate().GoToUrl("http://eaapp.somee.com/");
+                driver.Navigate().GoToUrl("http://eaapp.somee.com/");
+                // login
+                // IWebElement InkLogin = webDriver.FindElement(By.LinkText("Login"));
+                IWebElement InkLogin = driver.FindElement(By.LinkText("Login"));
+                InkLogin.Click();
+                // Username
+                //var txtUserName = webDriver.FindElement(By.Name("UserName"));
+                var txtUserName = driver.FindElement(By.Name("UserName"));
+                // Assertion
 
+                Assert.That(txtUserName.Displayed, Is.True);
+
+                txtUserName.SendKeys("admin");
+                //webDriver.FindElement(By.Name("Password")).SendKeys("password");
+                driver.FindElement(By.Name("Password")).SendKeys("password");
+                //webDriver.FindElement(By.XPath("//input[@value='Log in']")).Submit();
+                driver.FindElement(By.XPath("//input[@value='Log in']")).Submit();
+                //var InkEmployeeDetails = webDriver.FindElement(By.LinkText("Employee Details"));
+                var InkEmployeeDetails = driver.FindElement(By.LinkText("Employee Details"));
+                Assert.That(InkEmployeeDetails.Displayed, Is.True);
+               
+               
+            }
            
-
         }
-        IWebDriver webDriver = new ChromeDriver();
-        [TearDown]
+      
+        //IWebDriver webDriver = new ChromeDriver();
         
-        public void TearDown() => webDriver.Quit();
+       // [TearDown]
+        
+        //public void TearDown() => webDriver.Quit();
 
     }
 }
