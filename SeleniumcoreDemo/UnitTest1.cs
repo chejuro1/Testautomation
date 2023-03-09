3ce36472c9ae7b4e86e478da2ebce1f4c26385bf
Jules Chendjou <julestl@yahoo.fr>
2023-03-09 12:54:49 -0500

    update testcase

--- SeleniumcoreDemo/UnitTest1.cs
+++ SeleniumcoreDemo/UnitTest1.cs
@@ -28,26 +28,26 @@ namespace SeleniumcoreDemo
                 // Navigate to site 
 
                 //webDriver.Navigate().GoToUrl("http://eaapp.somee.com/");
-                driver.Navigate().GoToUrl("http://eaapp.somee.com/");
+                driver.Navigate().GoToUrl("https://www.google.ca/");
                 // login
-                // IWebElement InkLogin = webDriver.FindElement(By.LinkText("Login"));
-                IWebElement InkLogin = driver.FindElement(By.LinkText("Login"));
-                InkLogin.Click();
-                // Username
-                //var txtUserName = webDriver.FindElement(By.Name("UserName"));
-                var txtUserName = driver.FindElement(By.Name("UserName"));
-                // Assertion
+                // // IWebElement InkLogin = webDriver.FindElement(By.LinkText("Login"));
+                // IWebElement InkLogin = driver.FindElement(By.LinkText("Login"));
+                // InkLogin.Click();
+                // // Username
+                // //var txtUserName = webDriver.FindElement(By.Name("UserName"));
+                // var txtUserName = driver.FindElement(By.Name("UserName"));
+                // // Assertion
 
-                Assert.That(txtUserName.Displayed, Is.True);
+                // Assert.That(txtUserName.Displayed, Is.True);
 
-                txtUserName.SendKeys("admin");
-                //webDriver.FindElement(By.Name("Password")).SendKeys("password");
-                driver.FindElement(By.Name("Password")).SendKeys("password");
-                //webDriver.FindElement(By.XPath("//input[@value='Log in']")).Submit();
-                driver.FindElement(By.XPath("//input[@value='Log in']")).Submit();
-                //var InkEmployeeDetails = webDriver.FindElement(By.LinkText("Employee Details"));
-                var InkEmployeeDetails = driver.FindElement(By.LinkText("Employee Details"));
-                Assert.That(InkEmployeeDetails.Displayed, Is.True);
+                // txtUserName.SendKeys("admin");
+                // //webDriver.FindElement(By.Name("Password")).SendKeys("password");
+                // driver.FindElement(By.Name("Password")).SendKeys("password");
+                // //webDriver.FindElement(By.XPath("//input[@value='Log in']")).Submit();
+                // driver.FindElement(By.XPath("//input[@value='Log in']")).Submit();
+                // //var InkEmployeeDetails = webDriver.FindElement(By.LinkText("Employee Details"));
+                // var InkEmployeeDetails = driver.FindElement(By.LinkText("Employee Details"));
+                // Assert.That(InkEmployeeDetails.Displayed, Is.True);
                
                
             }
